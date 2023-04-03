9bf6cf09419f2ee1382152bc6a2f9438a3af6c8f
nick <nickrakas1993@hotmail.com>
2023-04-03 19:41:36 +0200

    Update

--- lib/book-manager-cdk-stack.ts
+++ lib/book-manager-cdk-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct } from 'constructs'
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 import * as path from 'path'
 
 export class BookManagerCdkStack extends cdk.Stack {
@@ -24,18 +25,32 @@ export class BookManagerCdkStack extends cdk.Stack {
 
     // Create Lambda to access DynamoDB Database
     const bookCatalogManager = new lambda.Function(this, 'BookCatalogManager', {
-      runtime: lambda.Runtime.PYTHON_3_9,
+      runtime: lambda.Runtime.NODEJS_16_X,
       functionName: 'book-catalog-manager',
       description: 'Supports CRUD operations for a book catalog',
       code: lambda.Code.fromAsset(path.join(__dirname, 'function')),
       handler: 'index.handler',
       environment: {
-        DYNAMODB_TABLE: booksCatalogTable.tableName
+        DYNAMODB_TABLE_NAME: booksCatalogTable.tableName
       }
     });
     
     // Allow Lambda to access DynamoDB table with Read/Write rights
     booksCatalogTable.grantReadWriteData(bookCatalogManager)
 
+    const api = new apigateway.RestApi(this, 'CRUDRestApi', {
+      restApiName: 'book-crud-api',
+      description: 'book-crud-api',
+      deploy: true
+    })
+
+    const bookResource = api.root.addResource('book')
+    bookResource.addMethod('POST', new apigateway.LambdaIntegration(bookCatalogManager), {
+      apiKeyRequired: false
+    })
+
+    bookResource.addMethod('GET', new apigateway.LambdaIntegration(bookCatalogManager), {
+      apiKeyRequired: false
+    });
   }
 }
