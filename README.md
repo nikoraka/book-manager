bd9c368398515a4afcec6003a60f9dd8c20deba1
nick <nickrakas1993@hotmail.com>
2023-04-04 16:10:36 +0200

    Updated README, optimized code

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Welcome to your CDK TypeScript project
+# Serverless Book Manager App
 
-This is a blank project for CDK development with TypeScript.
+## Overview
+This is a CDK App that creates a REST API, a DynamoDB table and 5 Lambda functions. One of the Lambda functions serves as a custom authorizer to the REST API while the other lambdas are created to support CRUD operations to the DynamoDB table. The Lambda authorizer takes into account that a Third Party Identity Provider is in place to offer an authentication token for any user that would like to make a request. A valid authorization token is the one with value `allow`.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Diagram
+![Visualization](./assets/visualization.png?raw=true "Visualization")
+
+## How to deploy AWS resources
+* Clone the repository
+* Run `npm install` to install relevant dependencies
+* Run `cdk deploy` to deploy the AWS resources
+
+## How to test
+* After successfull deployment, locate the REST API ID from the API Gateway console. It should be something similar to `018iiq6b19`
+* Lets say that we would like to register a new item of the following format to the DynamoDB table:
+```json
+{
+   "name": "<name of the book>",
+   "isbn": "<universal identifier of the book>",
+   "authors": "<authors of the book>",
+   "languages": "<languages the book is available>",
+   "countries": "<countries where the book is available>",
+   "numberOfPages": "<number of pages of the book>",
+   "releaseDate": "<release date of the book>",
+}
+```
+* In your terminal, execute bellow command:
+```console
+curl -X POST -H "Content-Type: application/json" -d '{ "isbn": "1", "name": "Robinson Crusoe", "authors": "Jules Verne", "languages": "French", "countries": "France", "numberOfPages": "100", "releaseDate": "1719"}' https://rest-api-id.execute-api.aws-region.amazonaws.com/prod/book --tlsv1.2 --header 'Accept: */*' --header 'Authorization: allow'
+``` 
 
 ## Useful commands
 
--- lib/book-manager-cdk-stack.ts
+++ lib/book-manager-cdk-stack.ts
@@ -22,69 +22,44 @@ export class BookManagerCdkStack extends cdk.Stack {
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
       }
     );
-
-    // Create Lambda to Put Item to DynamoDB Table
-    const putItemLambda = new lambda.Function(this, 'PutItemLambda', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      functionName: 'put-item-function',
-      description: 'Supports CRUD operations for a book catalog',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'functions/put-item')),
-      handler: 'index.handler',
-      environment: {
-        DYNAMODB_TABLE_NAME: booksCatalogTable.tableName
-      }
-    });
-    
+   
+    /*
+      Create Lambda to Put Items to Dynamodb Table
+      and grant relevant access
+    */
+    const putItemLambda = createLambdaStack(this, booksCatalogTable.tableName, 'put' )    
     booksCatalogTable.grant(putItemLambda, 'dynamodb:BatchWriteItem', 'dynamodb:PutItem', 'dynamodb:DescribeTable')
 
-    // Create Lambda to access DynamoDB Database
-    const deleteItemLambda = new lambda.Function(this, 'DeleteItemLambda', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      functionName: 'delete-item-function',
-      description: 'Supports CRUD operations for a book catalog',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'functions/delete-item')),
-      handler: 'index.handler',
-      environment: {
-        DYNAMODB_TABLE_NAME: booksCatalogTable.tableName
-      }
-    });
-
-    // Create Lambda to access DynamoDB Database
-    const updateItemLambda = new lambda.Function(this, 'UpdateItemLambda', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      functionName: 'update-item-function',
-      description: 'Supports CRUD operations for a book catalog',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'functions/update-item')),
-      handler: 'index.handler',
-      environment: {
-        DYNAMODB_TABLE_NAME: booksCatalogTable.tableName
-      }
-    });
-
-    const queryItemLambda = new lambda.Function(this, 'QueryItemLambda', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      functionName: 'query-item-function',
-      description: 'Supports CRUD operations for a book catalog',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'functions/query-item')),
-      handler: 'index.handler',
-      environment: {
-        DYNAMODB_TABLE_NAME: booksCatalogTable.tableName
-    }
-    });
-
-    // Allow Lambda to access DynamoDB table with Read/Write rights
-    booksCatalogTable.grantReadData(queryItemLambda)
-    booksCatalogTable.grant(putItemLambda, 'dynamodb:BatchWriteItem', 'dynamodb:PutItem', 'dynamodb:DescribeTable')
+    /*
+      Create Lambda to Delete Items from Dynamodb Table
+      and grant relevant access
+    */
+    const deleteItemLambda = createLambdaStack(this, booksCatalogTable.tableName, 'delete')  
     booksCatalogTable.grant(deleteItemLambda, 'dynamodb:DeleteItem', 'dynamodb:DescribeTable')
-    booksCatalogTable.grant(updateItemLambda,'dynamodb:BatchWriteItem', 'dynamodb:DescribeTable', 'dynamodb:UpdateItem')
 
+    /*
+      Create Lambda to Update Items in Dynamodb Table
+      and grant relevant access
+    */
+    const updateItemLambda = createLambdaStack(this, booksCatalogTable.tableName, 'update')
+    booksCatalogTable.grant(updateItemLambda,'dynamodb:BatchWriteItem', 'dynamodb:DescribeTable', 'dynamodb:UpdateItem')
+        
+    /*
+      Create Lambda to Query Items in Dynamodb Table
+      and grant relevant access
+    */
+    const queryItemLambda = createLambdaStack(this, booksCatalogTable.tableName, 'query')
+    booksCatalogTable.grantReadData(queryItemLambda)  
+
+    // Create a REST API
     const restApi = new apigateway.RestApi(this, 'CRUDRestApi', {
       restApiName: 'book-crud-api',
       description: 'REST API supporting CRUD operations for a book catalog',
       deploy: true
     })
+
     // Custom Lambda authoriser. Token needs to be retrieved from a Third Party App
-    const lambdaAuth = new lambda.Function( this, 'my-lambda-authoriser',{
+    const lambdaAuth = new lambda.Function( this, 'BookCatalogAuthorizer',{
       runtime: lambda.Runtime.NODEJS_14_X,
       functionName: 'book-catalog-authorizer',
       timeout: cdk.Duration.seconds(15),
@@ -94,12 +69,17 @@ export class BookManagerCdkStack extends cdk.Stack {
 
     const authorizer = new apigateway.TokenAuthorizer(
         this,
-        `my-lambda-authorizer`, {
+        `ApiTokenAuthorizer`, {
           handler: lambdaAuth
         }
     )
     const bookResource = restApi.root.addResource('book')   
-
+    
+    /*
+      In order to edit an item in the DynamoDB table,
+      authorization is needed. A token needs to be included
+      in the request header    
+    */
     bookResource.addMethod('POST', new apigateway.LambdaIntegration(putItemLambda), {
       apiKeyRequired: false,
       authorizer
@@ -110,22 +90,39 @@ export class BookManagerCdkStack extends cdk.Stack {
       authorizer
     })
 
-    bookResource.addMethod('GET', new apigateway.LambdaIntegration(queryItemLambda), {
-      apiKeyRequired: false,
-      requestParameters: {
-        "method.request.querystring.isbn": true,
-        "method.request.querystring.authors": true,
-        "method.request.querystring.countries": true,
-        "method.request.querystring.languages": true,
-        "method.request.querystring.name": true,
-        "method.request.querystring.numberOfPages": true,
-        "method.request.querystring.releaseDate": true
-      }
-    })
-
     bookResource.addMethod('PUT', new apigateway.LambdaIntegration(updateItemLambda), {
       apiKeyRequired: false,
       authorizer
     })
+
+    /*
+      For retrieving an item no authorization is needed,
+      i.e. no necessity to include a token in the request
+      Filtering options are offered based on languages. 
+      A value can be passed in the request as a query string
+    */
+    bookResource.addMethod('GET', new apigateway.LambdaIntegration(queryItemLambda), {
+      apiKeyRequired: false,
+      requestParameters: {
+        "method.request.querystring.languages": true
+      }
+    })
   }
+}
+
+function createLambdaStack(scope: Construct, tableName: string, option: string): cdk.aws_lambda.Function{
+  return new lambda.Function(scope, capitalizeFirstLetter(`${option}ItemLambda`), {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    functionName: `${option}-item-function`,
+    description: 'Supports CRUD operations for a book catalog',
+    code: lambda.Code.fromAsset(path.join(__dirname, `functions/${option}-item`)),
+    handler: 'index.handler',
+    environment: {
+      DYNAMODB_TABLE_NAME: tableName
+    }
+  })
+}
+
+function capitalizeFirstLetter(s: string) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
 }
